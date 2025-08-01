06afd92fde3442929914eb85ca5ad0ef905eb4bb
deverick <dncellassistec@gmail.com>
2025-07-31 21:15:10 -0300

    added safe route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,16 @@
+import { redirect } from "next/navigation"
 import { Container } from "@/components/container"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/auth"
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if(!session || !session.user){
+    redirect("/")
+  }
+ 
+
   return (
     <Container>
       <h1>Página Dashboard</h1>
