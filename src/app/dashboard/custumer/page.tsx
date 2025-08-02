54cbbc4ca744ad8c721b11c4f2d15664542bdc2e
deverick <dncellassistec@gmail.com>
2025-08-01 21:02:52 -0300

    created custumer page

--- src/app/dashboard/custumer/page.tsx
+++ src/app/dashboard/custumer/page.tsx
@@ -1,7 +1,9 @@
 import { Container } from "@/components/container";
 import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { CardCustumer } from "@/app/dashboard/custumer/components/card"
 
 export default async function Custumer() {
   const session = await getServerSession(authOptions);
@@ -12,10 +14,18 @@ export default async function Custumer() {
 
   return (
     <Container>
-      <main>
-        <div>
-          <h1>Página clientes</h1>
+      <main className="mt-9 mb-2">
+        <div className=" flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Meus clientes</h1>
+          <Link href="/dashboard/custumer/register" className="bg-blue-500 px-4 py-1 text-white font-medium rounded">
+            Novo cliente
+          </Link>
         </div>
+        <section className="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 mt-3">
+          <CardCustumer />
+          <CardCustumer />
+          <CardCustumer />
+        </section>
       </main>
     </Container>
   );
