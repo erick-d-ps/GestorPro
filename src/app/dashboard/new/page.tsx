106c757b0cba47d4b4a5c12d407b95dad51a3192
deverick <dncellassistec@gmail.com>
2025-08-01 21:08:02 -0300

    call log page created

---
+++ src/app/dashboard/new/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container";
+
+export default function NeuTicket() {
+  return (
+    <Container>
+      <div>
+        <h1>Registro de chamado</h1>
+      </div>
+    </Container>
+  );
+}
