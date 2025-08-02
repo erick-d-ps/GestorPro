3424b008bac1f4aa142a5647f096c9f1d430e722
deverick <dncellassistec@gmail.com>
2025-08-01 21:05:35 -0300

    crated new  resgiste page

---
+++ src/app/dashboard/custumer/register/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container";
+
+export default function RegisteCustumer(){
+   return(
+     <Container>
+        <div>
+            <h1>Página registro de clientes</h1>
+        </div>
+     </Container>
+   ) 
+}
