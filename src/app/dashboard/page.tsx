d5d1fd141aad31acd92f65f9f4e9a1f46051baa3
deverick <dncellassistec@gmail.com>
2025-08-01 21:13:45 -0300

    created dashboard page interface

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation"
 import { Container } from "@/components/container"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
+import Link from "next/link";
+import { TicketItem } from "./components/ticket";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
@@ -13,7 +15,28 @@ export default async function Dashboard() {
 
   return (
     <Container>
-      <h1>Página Dashboard</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Chamados</h1>
+          <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 text-white font-medium rounded ">
+            Novo chamado
+          </Link>
+        </div>
+
+        <table className="min-w-full my-2">
+          <thead>
+            <tr>
+              <th className="font-medium text-left pl-1">CLIENTE</th>
+              <th className="font-medium text-left hidden sm:block">DATA CADASTRO</th>
+              <th className="font-medium text-left">STATUS</th>
+              <th className="font-medium text-left">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TicketItem />
+          </tbody>
+        </table>
+      </main>
     </Container>
   );
 }
