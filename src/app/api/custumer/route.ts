bb9ddebc3f242121f7798b3d3282a5a654c7c569
deverick <dncellassistec@gmail.com>
2025-08-05 17:31:19 -0300

    created backend to register customer

---
+++ src/app/api/custumer/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import  PrismaClient  from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  const session = await getServerSession(authOptions);
+
+  if(!session || !session.user){
+    return NextResponse.json({ error: "Not authorized"}, { status: 401 })
+  }
+
+  const { name, email, phone, address, userId } = await request.json();
+
+  try{
+    await PrismaClient.custumer.create({
+        data:{
+          name,
+          email,
+          phone,
+          address: address ? address : "",
+          userId  
+        }
+    })
+
+    return NextResponse.json({message: "Cliente cadastrado com sucesso!"})
+
+  }catch(err){
+    return NextResponse.json({ error: "Failed crete new custumer"}, { status: 400 })
+  }
+
+}
