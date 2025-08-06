f3611da774387d85c249e48f46be545e7bec61f8
deverick <dncellassistec@gmail.com>
2025-08-06 16:47:32 -0300

    added refresh to load new users

--- src/app/dashboard/custumer/components/card/index.tsx
+++ src/app/dashboard/custumer/components/card/index.tsx
@@ -1,12 +1,13 @@
+import { CustumerPrps } from "@/utils/custumer.type"
 
-export function CardCustumer(){
+export function CardCustumer({custumer}: {custumer: CustumerPrps}){
     return(
       <article className="flex flex-col gap-2 p-2 bg-gray-100 border-1 border-gray-300 rounded-lg hover:scale-102 duration-300">
         <h2>
-           <a className="font-bold ">Nome: </a>mercado costa
+           <a className="font-bold ">Nome: </a>{custumer.name}
         </h2>
-        <p><a className="font-bold">Email: </a>modelo@modelo.com</p>
-        <p><a className="font-bold">Telefone: </a>999999999999</p>
+        <p><a className="font-bold">Email: </a>{custumer.email}</p>
+        <p><a className="font-bold">Telefone: </a>{custumer.phone}</p>
         <button className="bg-red-500 px-4 rounded text-white mt-2 self-start mb-1 ">
             Deletar
         </button>
