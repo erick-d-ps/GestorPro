a66ea7c04b913a98e8bc8713a6da3656f37aed8c
deverick <dncellassistec@gmail.com>
2025-08-06 18:57:57 -0300

    created the delete customer route

--- src/app/api/custumer/route.ts
+++ src/app/api/custumer/route.ts
@@ -3,6 +3,39 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import  PrismaClient  from "@/lib/prisma";
 
+export async function DELETE(request: Request){
+  const session = await getServerSession(authOptions)
+
+  if(!session || !session.user){
+    return NextResponse.json({ error: "Not authorized"}, { status: 401 })
+  }
+//Pegando da url o parametro 
+  const { searchParams } = new URL (request.url)
+
+  //pedindo para que pegue o id como parametro 
+  const userId = searchParams.get("id")
+
+  if(!userId){
+    return NextResponse.json({ error: "Custumer not found"}, { status: 400 })
+  }
+
+  
+  try{
+    await PrismaClient.custumer.delete({
+      where: {
+        id: userId as string
+      }
+    })
+
+    return NextResponse.json({message: "Cliente deletado com sucesso!"})
+
+  }catch(err){
+    console.log(err)
+    return NextResponse.json({ error: "Faild Delete custumer"}, { status: 400 })
+  }
+
+}
+
 export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
 
